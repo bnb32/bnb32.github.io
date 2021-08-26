aaeeab9d53f1393953596d78f84a61580f92c1be
bnb32 <bnb32@cornell.edu>
2021-08-26 15:33:00 -0600

    updates to bot details

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r ../../public_html/css ./
+cp -r ../../public_html/projects ./
+cp -r ../../public_html/docs ./
+cp -r ../../public_html/pics ./
+cp -r ../../public_html/php ./
+cp ../../public_html/index.html ./index.html
